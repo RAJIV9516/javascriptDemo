d707620b6410186927b53660dd922e41e4cf8722
Rajiv prajapati <rp3431458@gmail.com>
2024-12-03 12:46:45 +0530

    added

---
+++ November29/script.js
@@ -0,0 +1,24 @@
+var name="Rajiv Prajapati"
+
+function name1(){
+    console.log(name);
+    alert(name);
+    document.getElementById("demo").innerHTML=name;
+}
+name1();
+
+const name2=function(){
+    console.log(name);
+    alert(name);
+    document.getElementById("demo").innerHTML=name;
+
+}
+name2();
+
+var name3 =()=>{
+    console.log(name);
+    alert(name);
+    document.getElementById("demo").innerHTML=name;
+
+}
+name3();
---
+++ November3/script.js
@@ -0,0 +1,50 @@
+// write a loop  print odd number from to 100;
+
+function printoddnumber(){
+for(let i=1; i<=100 ;++i){
+    if(i%2){
+        console.log(i);
+    }
+}
+}
+printoddnumber();
+for(let i=1; i<100; ++i){
+    if(i%2!==0){
+        console.log(i);
+    }
+}
+
+// While loop
+
+let text = "";
+let y =1;
+while(y<=10){
+    text +=`The number is ${y} <br>`;
+    // text += "The number is "+ y + "<br>"; 
+    y++;
+
+}
+document.getElementById("demo").innerHTML = text;
+
+// do while loop
+let text1 = "";
+let z = 1;
+do{
+    text1 += `The number is ${z} <br>`;
+    console.log(z);
+    z++;
+}while(z<= 10);
+
+document.getElementById("demo").innerHTML = text1;
+
+
+function printStartPattern(row){
+for(let i=1; i<=row; ++i){
+    let stars = "";
+    for(let j=1; j<=i; j++){
+        stars ="*"
+        console.log
+
+    }
+}
+}
